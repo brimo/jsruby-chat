de12a368393e88f876b39397c9888b6861fcef61
Tamer Sherif <tamersherif.j@gmail.com>
2015-11-12 18:04:57 +0200

    use messages as a factory service

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 angular.module('jsrubyChat', [])
 
-.controller('MainCtrl', ['$scope', function($scope) {
-	$scope.messages = [{name: 'Tamer', text:'Test'}];
+.factory('messages', [function() {
+	var o = {
+		messages: []
+	};
+	o.create = function(msg) {
+		return o.messages.push(msg);
+	};
+	return o;
+}])
 
-	$scope.postMessage = function(){
+.controller('MainCtrl', ['$scope', 'messages', function($scope, messages) {
+	$scope.messages = messages;
+	$scope.postMessage = function() {
 		// validate message and name
 		if (!$scope.message || $scope.message === '' || !$scope.name || $scope.name === '') {
 			return;
 		}
-		$scope.messages.push({name: $scope.name, text: $scope.message})
+		$scope.messages.create({
+			name: $scope.name,
+			text: $scope.message
+		});
 		// reset message field
 		$scope.message = '';
 	}
