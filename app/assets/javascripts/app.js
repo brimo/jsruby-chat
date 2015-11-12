b11e6e303cb4d94de830792336ec2294482b5fc6
Tamer Sherif <tamersherif.j@gmail.com>
2015-11-12 18:48:53 +0200

    forgot to save changes in app.js

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -9,21 +9,4 @@ angular.module('jsrubyChat', ['ui.router', 'templates'])
 		});
 
 	$urlRouterProvider.otherwise('home');
-}])
-
-
-.controller('MainCtrl', ['$scope', 'messages', function($scope, messages) {
-	$scope.messages = messages;
-	$scope.postMessage = function() {
-		// validate message and name
-		if (!$scope.message || $scope.message === '' || !$scope.name || $scope.name === '') {
-			return;
-		}
-		$scope.messages.create({
-			name: $scope.name,
-			text: $scope.message
-		});
-		// reset message field
-		$scope.message = '';
-	}
 }]);
